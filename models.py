2119bf24479ae674f01514c74c2e08e19c64a8a7
moohyun <blacknwhite8954@gmail.com>
2020-03-25 11:13:41 +0900

    Update: more readable

--- models.py
+++ models.py
@@ -1,69 +1,92 @@
 import torch.nn as nn
 
+
 class Generator(nn.Module):
-    def __init__(self):
+    def __init__(self, in_channel=3, out_channel=3):
         super(Generator, self).__init__()
-
-        def UpDownBlock(mode, in_channel, out_channel,
-                        kernel_size, stride, padding, activation='relu', norm=True):
-            if mode == 'down':
-                layers = [nn.Conv2d(in_channel, out_channel, kernel_size=kernel_size, stride=stride, padding=padding)]
-            elif mode == 'up':
-                layers = [nn.ConvTranspose2d(in_channel, out_channel, kernel_size=kernel_size, stride=stride, 
-                                             padding=padding, output_padding=padding)]
-
-
-            if norm:
-                layers.append(nn.InstanceNorm2d(out_channel))
-            if activation == 'relu':
-                layers.append(nn.ReLU(inplace=True))
-            return layers
-
-        def ResnetBlock(in_channel, out_channel,
-                        kernel_size=4, stride=1, padding=1, activation='relu', norm=True):
-            layers = []
-            for i in range(2):
-                layers.extend([nn.ReflectionPad2d(padding),
-                               nn.Conv2d(in_channel, out_channel, kernel_size=kernel_size, stride=stride)])
-                if norm:
-                    layers.append(nn.InstanceNorm2d(out_channel))
-                if activation == 'relu' and i != 1:
-                    layers.append(nn.ReLU(inplace=True))
-            return layers
         
         self.down_sample = nn.Sequential(nn.ReflectionPad2d(3),
-                                         *UpDownBlock('down', 3, 64, 7, 1, 0),
-                                         *UpDownBlock('down', 64, 128, 3, 2, 1),
-                                         *UpDownBlock('down', 128, 256, 3, 2, 1))
+                                         DownBlock(3, 64, 7, 1, 0),
+                                         DownBlock(64, 128, 3, 2, 1),
+                                         DownBlock(128, 256, 3, 2, 1))
 
-        b1 = nn.Sequential(*ResnetBlock(256, 256, 3, 1))
-        b2 = nn.Sequential(*ResnetBlock(256, 256, 3, 1))
-        b3 = nn.Sequential(*ResnetBlock(256, 256, 3, 1))
-        b4 = nn.Sequential(*ResnetBlock(256, 256, 3, 1))
-        b5 = nn.Sequential(*ResnetBlock(256, 256, 3, 1))
-        b6 = nn.Sequential(*ResnetBlock(256, 256, 3, 1))
-        b7 = nn.Sequential(*ResnetBlock(256, 256, 3, 1))
-        b8 = nn.Sequential(*ResnetBlock(256, 256, 3, 1))
-        b9 = nn.Sequential(*ResnetBlock(256, 256, 3, 1))
-        self.resnet_blocks = nn.Sequential(b1, b2, b3, b4, b5, b6, b7, b8, b9)
+        self.resnet_blocks = []
+        for _ in range(9):
+            self.resnet_blocks += [ResnetBlock(256, 256, 3, 1)]
+        self.resnet_blocks = nn.Sequential(*self.resnet_blocks)
 
-        self.up_sample = nn.Sequential(*UpDownBlock('up', 256, 128, 3, 2, 1),
-                                       *UpDownBlock('up', 128, 64, 3, 2, 1),
+        self.up_sample = nn.Sequential(UpBlock(256, 128, 3, 2, 1),
+                                       UpBlock(128, 64, 3, 2, 1),
                                        nn.ReflectionPad2d(3),
-                                       nn.Conv2d(64, 3, kernel_size=9, stride=1),
+                                       nn.Conv2d(64, 3, kernel_size=7, stride=1),
                                        nn.Tanh())
 
-    def forward(self, x, pose):
+    def forward(self, x):
         x = self.down_sample(x)
         x = self.resnet_blocks(x)
         out = self.up_sample(x)
         return out
 
 
+class UpBlock(nn.Module):
+    def __init__(self, in_channel, out_channel, kernel_size, stride, padding, activation='relu', norm=True):
+        super(UpBlock, self).__init__()
+        layers = [nn.ConvTranspose2d(in_channel, out_channel, kernel_size=kernel_size, stride=stride, 
+                                     padding=padding, output_padding=padding)]
+        if norm:
+            layers.append(nn.InstanceNorm2d(out_channel))
+        if activation == 'relu':
+            layers.append(nn.ReLU(inplace=True))
+        self.upblock = nn.Sequential(*layers) 
+
+    def forward(self, x):
+        out = self.upblock(x)
+        return out
+
+class DownBlock(nn.Module):
+    def __init__(self, in_channel, out_channel, kernel_size, stride, padding, activation='relu', norm=True):
+        super(DownBlock, self).__init__()
+        self.downblock = self._make_downblock(in_channel, out_channel, kernel_size, stride, padding, activation, norm)
+
+    def _make_downblock(self, in_channel, out_channel, kernel_size, stride, padding, activation, norm):
+        layers = [nn.Conv2d(in_channel, out_channel, kernel_size=kernel_size, stride=stride, padding=padding)]
+        if norm:
+            layers.append(nn.InstanceNorm2d(out_channel))
+        if activation == 'relu':
+            layers.append(nn.ReLU(inplace=True))
+        return nn.Sequential(*layers) 
+
+    def forward(self, x):
+        return self.downblock(x)
+
+
+class ResnetBlock(nn.Module):
+    def __init__(self, in_channel, out_channel, kernel_size=4, stride=4, padding=1, activation='relu', norm=True):
+        super(ResnetBlock, self).__init__()
+        self.conv_block = self.build_conv_block(in_channel, out_channel, kernel_size, stride, padding, activation, norm)
+
+    def build_conv_block(self, in_channel, out_channel,
+                         kernel_size=4, stride=1, padding=1, activation='relu', norm=True):
+        layers = []
+        for i in range(2):
+            layers += [nn.ReflectionPad2d(padding),
+                       nn.Conv2d(in_channel, out_channel, kernel_size=kernel_size, stride=stride)]
+            if norm:
+                layers.append(nn.InstanceNorm2d(out_channel))
+            if activation == 'relu' and i != 1:
+                layers.append(nn.ReLU(inplace=True))
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        out = x + self.conv_block(x)
+        return out
+
+
 class Discriminator(nn.Module):
-    def __init__(self, num_classes):
+    def __init__(self, num_classes, last_activation='sigmoid'):
         super(Discriminator, self).__init__()
         self.num_classes = num_classes
+        self.last_activation = last_activation
 
         def block(layer, in_channel, out_channel,
                   kernel_size=4, stride=2, padding=1, activation='lrelu', norm=True):
@@ -102,12 +125,11 @@ class Discriminator(nn.Module):
                                      *block('conv', 256, 512),
                                      *block('conv', 512, 512))
 
-        self.classifier = nn.Sequential(*block('linear', 512*3*2, 1024, norm=False),
+        self.classifier = nn.Sequential(*block('linear', 512*2*2, 1024, norm=False),
                                         *block('linear', 1024, 512, norm=False),
                                         *block('linear', 512, self.num_classes, norm=False, activation='none'))
 
-#        self.discriminator = nn.Sequential(*block('linear', 512*3*2, 1, norm=False, activation='sigmoid'))
-        self.discriminator = nn.Sequential(*block('linear', 512*3*2, 1, norm=False, activation='none'))
+        self.discriminator = nn.Sequential(*block('linear', 512*2*2, 1, norm=False, activation=self.last_activation))
 
     def forward(self, x):
         out = self.feature(x)
@@ -124,3 +146,12 @@ def weights_init(m):
     elif classname.find('BatchNorm') != -1:
         m.weight.data.normal_(0.0, 0.02)
         m.bias.data.fill_(0)
+
+if __name__ == '__main__':
+    import torch
+    inputs = torch.Tensor(16,3,128,128)
+    G = Generator()
+    print(G)
+    G = G.to('cuda')
+    inputs = inputs.to('cuda')
+    print(G(inputs).shape)

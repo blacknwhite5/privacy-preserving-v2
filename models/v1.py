6fcce63af335b17c37be0da31152b7a165015895
moohyun <blacknwhite8954@gmail.com>
2020-04-06 15:17:03 +0900

    Update: input with landmark in resnet_9blocks, replace transpose to interpolate

--- models/v1.py
+++ models/v1.py
@@ -1,12 +1,13 @@
 import torch.nn as nn
+import torch
 
 
 class resnet_9blocks(nn.Module):
-    def __init__(self, in_channel=3, out_channel=3):
+    def __init__(self, in_channel=4, out_channel=3, last_activation='tanh'):
         super(resnet_9blocks, self).__init__()
         
-        self.down_sample = nn.Sequential(nn.ReflectionPad2d(3),
-                                         DownBlock(3, 64, 7, 1, 0),
+        self.down_sample = nn.Sequential(nn.ReflectionPad2d(in_channel),
+                                         DownBlock(in_channel, 64, 7, 1, 0),
                                          DownBlock(64, 128, 3, 2, 1),
                                          DownBlock(128, 256, 3, 2, 1))
 
@@ -15,16 +16,42 @@ class resnet_9blocks(nn.Module):
             self.resnet_blocks += [ResnetBlock(256, 256, 3, 1)]
         self.resnet_blocks = nn.Sequential(*self.resnet_blocks)
 
-        self.up_sample = nn.Sequential(UpBlock(256, 128, 3, 2, 1),
-                                       UpBlock(128, 64, 3, 2, 1),
-                                       nn.ReflectionPad2d(3),
-                                       nn.Conv2d(64, 3, kernel_size=7, stride=1),
-                                       nn.Tanh())
+#        up_blocks = [UpBlock(256, 128, 3, 2, 1),
+#                     UpBlock(128, 64, 3, 2, 1),
+#                     nn.ReflectionPad2d(3),
+#                     nn.Conv2d(64, 3, kernel_size=7, stride=1)]
+#
+#        if last_activation == 'tanh':
+#            up_blocks.append(nn.Tanh())
+#        elif last_activation == 'none':
+#            pass
+#        else:
+#            raise NotImplementedError("should be 'tanh' or 'none'")
+#
+#        self.up_sample = nn.Sequential(*up_blocks)
+        
+        self.pad = nn.ReflectionPad2d(3)
+        self.conv1 = nn.Conv2d(256, 128, kernel_size=1, stride=1)
+        self.conv2 = nn.Conv2d(128, 64, kernel_size=1, stride=1)
+        self.conv3 = nn.Conv2d(64, 3, kernel_size=7, stride=1)
+        self.relu = nn.ReLU(inplace=True)
+        self.tanh = nn.Tanh()
 
-    def forward(self, x):
+    # TODO: landmark, layernorm
+    def forward(self, x, face_info):
+        x = torch.cat([x, face_info], dim=1)
         x = self.down_sample(x)
         x = self.resnet_blocks(x)
-        out = self.up_sample(x)
+#        out = self.up_sample(x)
+        x = nn.functional.interpolate(x, (64, 64))
+        x = self.conv1(x)
+        x = self.relu(x)
+        x = nn.functional.interpolate(x, (128, 128))
+        x = self.conv2(x)
+        x = self.relu(x)
+        x = self.pad(x)
+        x = self.conv3(x)
+        out = self.tanh(x)
         return out
 
 
@@ -85,7 +112,7 @@ class ResnetBlock(nn.Module):
 # TODO : need to modify
 class PatchGAN(nn.Module):
     def __init__(self, num_classes, last_activation='sigmoid'):
-        super(Patchnet, self).__init__()
+        super(PatchGAN, self).__init__()
         self.num_classes = num_classes
         self.last_activation = last_activation
 

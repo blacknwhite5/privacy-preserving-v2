774feaabbf0e60bbb0b1a623819ef7665348ce17
moohyun <blacknwhite8954@gmail.com>
2020-04-20 14:21:19 +0900

    Update: trend line

--- log2graph.py
+++ log2graph.py
@@ -1,16 +1,22 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import sys
 
 def parse_log(logfile):
     with open(logfile, 'r') as f:
         data = f.read()
 
     epoch, loss_d, loss_g = [], [], []
-    for i, line in enumerate(data.split('\n')[1:-1]):
+    for i, line in enumerate(data.split('\n')[:-1]):
+        if i == 0:
+            line = line.split(',')[2]
+            batch_size = int(line.split('=')[1])
+            continue
+            
         line = line.split()
         
 #        ep = int(line[0].split(':')[1])
-        ep = 20000 * i
+        ep = (20000 * i) // batch_size
         l_d = float(line[2].split(':')[1])
         l_g = float(line[3].split(':')[1])
         epoch.append(ep)
@@ -24,8 +30,21 @@ def draw_results(epoch, losses_D, losses_G, show=False):
     modula = len(axes_cycle) 
     
     plt.figure() 
-    plt.plot(epoch, losses_D, color=axes_cycle[0], label='loss_D') 
-    plt.plot(epoch, losses_G, color=axes_cycle[2], label='loss_G') 
+    plt.plot(epoch, losses_D, color=axes_cycle[0], alpha=0.5) 
+    plt.plot(epoch, losses_G, color=axes_cycle[2], alpha=0.5) 
+
+    new_x = np.linspace(epoch[0], epoch[-1])
+
+    coefficients = np.polyfit(epoch, losses_D, 10)
+    poly = np.poly1d(coefficients)
+    new_y = poly(new_x)
+    plt.plot(new_x, new_y, color=axes_cycle[0], label='loss_D') 
+
+    coefficients = np.polyfit(epoch, losses_G, 10)
+    poly = np.poly1d(coefficients)
+    new_y = poly(new_x)
+    plt.plot(new_x, new_y, color=axes_cycle[2], label='loss_G') 
+
 #    plt.plot(epoch[np.argmin(losses_D)], min(losses_D), 'o', color=axes_cycle[3], label='min D loss') 
 #    plt.plot(epoch[np.argmin(losses_G)], min(losses_G), 'o', color=axes_cycle[4], label='min G loss') 
     plt.xlabel("iteration") 
@@ -42,6 +61,9 @@ def show_graph(logfile, show):
     draw_results(*parse_log(logfile), show)
 
 if __name__ == '__main__':
-    LOG_FILE = 'log.txt'
+    if len(sys.argv) == 2:
+        LOG_FILE = sys.argv[1]
+    else:
+        LOG_FILE = 'log.txt'
     epoch, loss_d, loss_g = parse_log(LOG_FILE)
     draw_results(epoch, loss_d, loss_g)
